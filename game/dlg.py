086d6150696516cc90a7a8f4a087a6e2df51dc2f
darrickyee <darrick.yee@gmail.com>
2020-03-09 16:36:46 -0400

    Updates

--- game/dlg.py
+++ game/dlg.py
@@ -1,106 +1,19 @@
 # %%
-from functools import reduce
-from typing import List, Dict, Any
-from abc import ABC, abstractmethod
 
 
-class AbstractNode(ABC):
+dt0 = DlgText('dt0', text='Hi!', edges=[{'key': 'dt1'}])
+dt1 = DlgText('dt1', edges=[{'key': 'dr1'}], text='H ow are you!')
+dr1 = DlgResponse('dr1', [{'key': 'dt2', 'text': 'Fyne'}, {
+                  'key': 'dt3', 'text': 'Bad'}, {
+    'key': 'dt3', 'text': 'Ok', 'condition': True}])
+dt2 = DlgText('dt2', text='Gewd!', edges=[{'key': 'db1'}])
+dt3 = DlgText('dt3', [{'key': 'dt0'}], text='Try again!')
 
-    def __init__(self, nodeid: str, edges: List[Dict[str, Any]] = None):
-        self.nodeid = nodeid
-        self.edges = edges or []
+db1 = DlgBranch('db1', edges=[{'key': 'dt0', 'condition': True}, {
+                'key': 'dt1', 'condition': False}])
 
-    @abstractmethod
-    def next(self, index=0):
-        pass
-
-
-class DlgText(AbstractNode):
-
-    def __init__(self, nodeid, edges=None, text=''):
-        super().__init__(nodeid, edges)
-        self.text = text
-
-    def next(self, index=0):
-        return self.edges[0].get('node', None) if self.edges else None
-
-
-class DlgResponse(AbstractNode):
-
-    def __init__(self, nodeid, edges=None):
-        super().__init__(nodeid, edges)
-
-    @property
-    def validEdges(self) -> List[Dict[str, Any]]:
-        return [edge for edge in self.edges if edge.get('condition', True)]
-
-    @property
-    def responses(self) -> List[str]:
-        return [edge.get('text', None) for edge in self.validEdges]
-
-    def next(self, index=0):
-        return self.validEdges[index].get('node', None) if self.validEdges and index < len(self.validEdges) else None
-
-
-class DlgBranch(AbstractNode):
-
-    def __init__(self, nodeid, edges=None):
-        super().__init__(nodeid, edges)
-
-    def next(self, index=0):
-        for edge in self.edges:
-            if edge.get('condition', False) or edge == self.edges[-1]:
-                return edge.get('node', None)
-
-        return None
-
-
-def displayDlg(text, responses):
-    if text:
-        print(text)
-
-    for i, resp in enumerate(responses):
-        print(f'[{i+1}]\t{resp}')
-
-    sel = input()
-    try:
-        sel = min(max(0, int(sel)-1), len(responses)-1)
-    except ValueError:
-        sel = 0
-
-    return sel
-
-
-def playDlg(graph):
-
-    currnode = graph['0']
-    nextidx = 0
-
-    while currnode:
-        text = currnode.text if hasattr(currnode, 'text') else None
-        responses = currnode.responses if hasattr(
-            currnode, 'responses') else []
-
-        if text or responses:
-            nextidx = displayDlg(text, responses)
-
-        currnode = graph.get(currnode.next(nextidx), None)
-
-# %%
-
-
-dt0 = DlgText('dt0', text='Hi!', edges=[{'node': 'dt1'}])
-dt1 = DlgText('dt1', edges=[{'node': 'dr1'}], text='H ow are you!')
-dr1 = DlgResponse('dr1', [{'node': 'dt2', 'text': 'Fyne'}, {
-                  'node': 'dt3', 'text': 'Bad'}, {
-    'node': 'dt3', 'text': 'Ok', 'condition': True}])
-dt2 = DlgText('dt2', text='Gewd!', edges=[{'node': 'db1'}])
-dt3 = DlgText('dt3', [{'node': 'dt0'}], text='Try again!')
-
-db1 = DlgBranch('db1', edges=[{'node': 'dt0', 'condition': True}, {
-                'node': 'dt1', 'condition': False}])
-
-dg = {n.nodeid: n for n in (dt0, dt1, dr1, dt2, dt3, db1)
+#%%
+dg = {n.key: n for n in (dt0, dt1, dr1, dt2, dt3, db1)
       }
 dg['0'] = dt0
 
--- game/dlg/node.py
+++ game/dlg/node.py
@@ -1,11 +1,12 @@
+import json
 from typing import List, Dict, Any
 from abc import ABC, abstractmethod
 
 
 class AbstractNode(ABC):
 
-    def __init__(self, nodeid: str, edges: List[Dict[str, Any]] = None):
-        self.nodeid = nodeid
+    def __init__(self, key: str, edges: List[Dict[str, Any]] = None):
+        self.key = key
         self.edges = edges or []
 
     @abstractmethod
@@ -13,17 +14,20 @@ class AbstractNode(ABC):
         pass
 
     def as_dict(self):
-        return {'nodeid': self.nodeid, 'edges': self.edges}
+        return {'key': self.key, 'edges': self.edges}
+
+    def __repr__(self):
+        return f'{self.__class__.__name__}< {str(self.as_dict())} >'
 
 
 class DlgText(AbstractNode):
 
-    def __init__(self, nodeid, edges=None, text=''):
-        super().__init__(nodeid, edges)
+    def __init__(self, key, edges=None, text=''):
+        super().__init__(key, edges)
         self.text = text
 
     def next(self, index=0):
-        return self.edges[0].get('node', None) if self.edges else None
+        return self.edges[0].get('key', None) if self.edges else None
 
     def as_dict(self):
         return {**super().as_dict(), 'text': self.text}
@@ -40,7 +44,7 @@ class DlgResponse(AbstractNode):
         return [edge.get('text', None) for edge in self.validEdges]
 
     def next(self, index=0):
-        return self.validEdges[index].get('node', None) if self.validEdges and index < len(self.validEdges) else None
+        return self.validEdges[index].get('key', None) if self.validEdges and index < len(self.validEdges) else None
 
 
 class DlgBranch(AbstractNode):
@@ -48,9 +52,51 @@ class DlgBranch(AbstractNode):
     def next(self, index=0):
         for edge in self.edges:
             if edge.get('condition', False) or edge == self.edges[-1]:
-                return edge.get('node', None)
+                return edge.get('key', None)
 
         return None
+
+
+def omit(key, d: Dict):
+    _d = d.copy()
+    if key in _d:
+        del _d[key]
+
+    return _d
+
+
+class DlgGraph:
+
+    nodetypes = {
+        DlgText: 'text',
+        DlgResponse: 'response',
+        DlgBranch: 'branch'
+    }
+
+    def __init__(self, rootkey, nodelist: List[AbstractNode] = None):
+        self.nodes = self._loadNodes(rootkey, nodelist or [])
+
+    def _loadNodes(self, key, nodelist: List[AbstractNode]):
+        nodes = {
+            node.key: node for node in nodelist if isinstance(node, AbstractNode)}
+        nodes['root'] = nodes[key]
+        return nodes
+
+    def export(self):
+        nodes = [{'nodetype': self.__class__.nodetypes.get(type(
+            node), 'UNKNOWN'), **omit('edges', node.as_dict())} for node in self.nodes.values()]
+        edges = [{'fromkey': node.key, **edge}
+                 for node in self.nodes.values() for edge in node.edges]
+        return {'nodes': nodes, 'edges': edges}
+
+    def getNode(self, key):
+        return self.nodes.get(key, None)
+
+
+def importGraph(nodes, edges):
+    ntypes = dict(map(lambda x: x[::-1], self.__class__.nodetypes.items()))
+    _nodes = [ntypes[n['nodetype']](**omit('nodetype', n)) for n in nodes]
+    return _nodes
 
 
 def displayDlg(text, responses):
